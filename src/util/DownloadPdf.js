d005a8c348b041e3e0bec8131d48b7fc7d5cf9c1
adriankiezik <kiezikadrian@gmail.com>
2022-04-22 19:46:48 +0200

    Create pdf generator

--- src/util/DownloadPdf.js
+++ src/util/DownloadPdf.js
@@ -1,3 +1,7 @@
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+import latinize from "latinize";
+
 const DownloadPdf = (routeNavInfo) => {
   if (routeNavInfo == null) {
     alert(
@@ -6,7 +10,42 @@ const DownloadPdf = (routeNavInfo) => {
     return;
   }
 
-  console.log(routeNavInfo);
+  const doc = new jsPDF();
+
+  const tableColumn = [
+    { header: "nr", dataKey: "nr" },
+    { header: "Czas", dataKey: "time" },
+    { header: "Odleglosc", dataKey: "distance" },
+    { header: "Instrukcja", dataKey: "instruction" },
+  ];
+  const tableRows = [];
+
+  routeNavInfo.instructions.forEach((instruction, index) => {
+    const instructionData = {
+      nr: ++index,
+      // Convert polish characters with latin ones,
+      // since jspdf doesn't support them
+      instruction: latinize(instruction.text),
+      time: Math.round(instruction.time / 60) + " min",
+      distance:
+        instruction.distance >= 1000
+          ? (instruction.distance / 1000).toFixed(1) + " km"
+          : Math.round(instruction.distance) + " m",
+    };
+    tableRows.push(instructionData);
+  });
+
+  doc.autoTable({
+    columns: tableColumn,
+    body: tableRows,
+    margin: { top: 20 },
+    didDrawPage: () => {
+      doc.text("Nawigacja", 14, 15);
+    },
+  });
+  doc.setFont("Helvetica");
+
+  doc.save("nawigacja.pdf");
 };
 
 export default DownloadPdf;
